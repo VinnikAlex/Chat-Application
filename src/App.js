480aa733cf0ba750001132f47617b70c4ed31872
Alexander Vinnik <82071984+Sincroniiza@users.noreply.github.com>
2022-08-21 12:58:48 +1000

    Design update / skeleton brainstorm

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://ilearn.mq.edu.au/mod/page/view.php?id=7131942"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          COMP3120 Individual Project
-        </a>
-      </header>
+      <div className="u-full-width">
+        <div className="row">
+          <div className="three columns">
+            <div className="authentication">
+              <button class="button-primary">Login</button>
+              <button class="button-primary">Register</button>
+            </div>
+            <div className="conversation">
+              {/* <input className="u-full-width" placeholder="Search..."></input> */}
+              <div className="tempdiv"></div>
+              {/* fetch your conversations here: */}
+              {/* <div>
+                {units.map((unit) => (
+                  <li key={unit.id}>
+                    {" "}
+                    {unit.id} - {unit.code} - {unit.title} - {unit.offering}
+                    <button value={unit.id} onClick={handleUnitDeletion}>
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </div> */}
+            </div>
+            <button class="button-primary">Sign Out</button>
+          </div>
+          <div className="nine columns">
+            <h1>Hello nine column</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
