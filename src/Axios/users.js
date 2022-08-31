a0fda0121d93fb4ffb48ea393cabb0cc58d36faa
Alexander Vinnik <82071984+Sincroniiza@users.noreply.github.com>
2022-08-31 10:12:35 +1000

    Fixed webpack errors

--- src/Axios/users.js
+++ src/Axios/users.js
@@ -1,19 +1,18 @@
 /** @format */
 
 import axios from "axios";
-// import jwt from " jsonwebtoken";
 
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 // const baseURL = "http://localhost:8102/";
 
 const getUser = (user) => {
-  //   const token = jwt.sign(user, process.env.SECRET);
+  const token = jwt.verify(user, process.env.SECRET);
 
   return axios
     .get("http://localhost:8102/auth/", {
       headers: {
-        authorization: "Basic 630ea12ff0bd4551c9a669b8",
+        authorization: "Basic " + token,
       },
       username: user,
     })
